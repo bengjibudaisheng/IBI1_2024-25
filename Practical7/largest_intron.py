35d9f03cafcdc25eba1ac227ddac68235b9d5891
bengjibudaisheng <yiyang1.24@intl.zju.edu.cn>
2025-04-07 15:20:11 +0100

    practical

--- Practical7/largest_intron.py
+++ Practical7/largest_intron.py
@@ -1,5 +1,9 @@
 import re
+
+# Input the sequence
 seq ='ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA'
+
+# Find the longest sequence that meets the conditions and calculate its length.
 intron=re.findall(r'GT.+AG',seq)
 length=len(intron[0])
 print(length)
