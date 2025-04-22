58f3437b9f79b9e2f39ec77b4b31a47253b56d9a
bengjibudaisheng <yiyang1.24@intl.zju.edu.cn>
2025-04-22 21:00:32 +0800

    practical

--- Practical5/Programming.py
+++ Practical5/Programming.py
@@ -1,15 +1,17 @@
-# import matplotlib.pyplot to create graghs
+# Import matplotlib.pyplot to create graghs.
 import matplotlib.pyplot as plt
 
-# difine variables you need when create a bar gragh
 LanguagePercentage={'JavaScript':62.3,'HTML':52.9,'Python':51,'SQL':51,'TypeScript':38.5}
+print(LanguagePercentage)
+
+# Define variables you need when create a bar gragh.
 languages=list(LanguagePercentage.keys())
 percentages=list(LanguagePercentage.values())
 
-# create a bar gragh
+# Create a bar gragh.
 plt.bar(languages,percentages)
 
-# set the labels and title of the bar gragh
+# Set the labels and title of the bar gragh.
 plt.xlabel('Promgramming languages')
 plt.ylabel('Percentages of developers')
 plt.title('Programming language popularity')
